6a93988de302bb3a0010e9a1357e1053a78b8c6a
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-04 16:11:56 -0600

    Se terminó favoritos

--- assets/scripts/script-favoritos.js
+++ assets/scripts/script-favoritos.js
@@ -1,3 +1,30 @@
-function clickFavoritos(valor){
-    valor.src ="../resources/img/corazon lleno.png";
+var contacto1 = false;
+var contacto2 = false;
+var contacto3 = false;
+function clickFavoritos(valor, numeroContacto) {
+    if (numeroContacto==1) {
+        if (contacto1 != true) {
+            valor.src = "../resources/img/corazon lleno.png";
+            contacto1 = true;
+        } else {
+            valor.src = "../resources/img/corazon.png";
+            contacto1 = false;
+        }
+    }else if(numeroContacto==2){
+        if (contacto2 != true) {
+            valor.src = "../resources/img/corazon lleno.png";
+            contacto2 = true;
+        } else {
+            valor.src = "../resources/img/corazon.png";
+            contacto2 = false;
+        }
+    }else if(numeroContacto==3){
+        if (contacto3 != true) {
+            valor.src = "../resources/img/corazon lleno.png";
+            contacto3 = true;
+        } else {
+            valor.src = "../resources/img/corazon.png";
+            contacto3 = false;
+        }
+    }
 }
