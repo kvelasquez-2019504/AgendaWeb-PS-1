3b868054d5f44da540daec5ddc6276ac6ed44175
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-04 23:30:17 -0600

    Se agrego creacion de cuenta

--- assets/scripts/script-index.js
+++ assets/scripts/script-index.js
@@ -1,9 +1,34 @@
-function iniciarSesion(){
-    let usuario = document.getElementById("txtUser").value;
-    let clave = document.getElementById("txtPassword").value;
-    if(usuario=="Admin"&&clave=="123"){
+var listaRegistro =[{nombre:"kenneth",clave:123},{nombre:"bryan",clave:456}];
+let usuario="";
+let clave="";
+let contador =0;
+
+function guardarCuenta(){
+    let user = document.getElementById("usuario").value;
+    let password = document.getElementById("clave").value;
+    console.log(user,password);
+    listaRegistro.push({nombre:user,clave:password});
+    console.log(listaRegistro);
+}
+
+function verificacion(persona){
+    if(usuario == persona.nombre&&persona.clave==clave){
+        localStorage.setItem("usuario", usuario);
+        localStorage.setItem("contrasena", clave);
         document.location.href ="./assets/pages/contacto.html";
     }else{
-        alert("Usuario o contraseña inválidos");
+        contador++;
+        if(contador==listaRegistro.length){
+            contador=0;
+            alert("Usuario o contraseña inválidos");
+        }
+        
     }
 }
+
+function iniciarSesion(){
+    usuario = document.getElementById("txtUser").value;
+    clave = document.getElementById("txtPassword").value;
+    listaRegistro.forEach(verificacion);
+}
+
