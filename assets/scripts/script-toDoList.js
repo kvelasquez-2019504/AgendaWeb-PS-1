784b809ed2103ecebc34687a522238745e7e661e
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-05 02:08:38 -0600

    Se agregó funcion elimnar

--- assets/scripts/script-toDoList.js
+++ assets/scripts/script-toDoList.js
@@ -11,8 +11,8 @@ function agregarTarea(){
     var cell1 = row.insertCell(0);
     cell1.innerHTML = tareaNueva;
     var cell2 = row.insertCell(1);
-    cell2.innerHTML = '<button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editTarea" onclick="editarTarea(this)">EDITAR</button><button class="btn btn-dark">ELIMINAR</button>';
-    
+    cell2.innerHTML = '<button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editTarea" onclick="editarTarea(this)">EDITAR</button><button class="btn btn-dark" onclick=eliminarTarea(this)>ELIMINAR</button>';
+    tareaNueva.innerHTML ="";
 }
 
 function editarTarea(tarea){
@@ -23,4 +23,13 @@ function editarTarea(tarea){
 function modificarTarea(){
     var txtEditTarea = document.getElementById("txtEditTarea").value;
     fila.cells[0].innerHTML = txtEditTarea;
+}
+
+function eliminarTarea(tarea) {
+    var opcion = prompt("¿Está seguro de eliminar?\nY/N");
+    console.log(opcion);
+    if(opcion.toUpperCase()=="Y"){
+        var eliminarFila = tarea.parentNode.parentNode;
+        eliminarFila.parentNode.removeChild(eliminarFila);
+    }
 }
