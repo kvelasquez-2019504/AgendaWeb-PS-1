54cfa70c86853a1c63abeac309e5d5ae1166e802
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-05 01:56:11 -0600

    Se agregó funcion editar

--- assets/scripts/script-toDoList.js
+++ assets/scripts/script-toDoList.js
@@ -1,18 +1,26 @@
+var fila;
+var tarea;
 function irAToDo(){
     document.location.href = "../pages/toDoList.html";
 }
 
 function agregarTarea(){
     var tareaNueva= document.getElementById("txtTarea").value;
-    var table = document.getElementById("tblTareas");
-    var row = table.insertRow(1);
+    var tblTareas = document.getElementById("tblTareas");
+    var row = tblTareas.insertRow(1);
     var cell1 = row.insertCell(0);
     cell1.innerHTML = tareaNueva;
     var cell2 = row.insertCell(1);
-    cell2.innerHTML = '<button>EDITAR</button> <button>ELIMINAR</button>';
-    document.getElementById("tblTareas").value = "";
+    cell2.innerHTML = '<button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editTarea" onclick="editarTarea(this)">EDITAR</button><button class="btn btn-dark">ELIMINAR</button>';
+    
 }
 
-function editarTarea(){
-    
+function editarTarea(tarea){
+    fila = tarea.parentNode.parentNode;
+    tarea = fila.cells[0].innerHTML;
+}
+
+function modificarTarea(){
+    var txtEditTarea = document.getElementById("txtEditTarea").value;
+    fila.cells[0].innerHTML = txtEditTarea;
 }
