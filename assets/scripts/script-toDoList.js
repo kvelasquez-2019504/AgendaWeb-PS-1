27391f9a94160c401e43a63fc6b2f9431dde3c93
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2024-02-05 01:23:22 -0600

    Se agregó funcionalidad de agregar tareas

--- assets/scripts/script-toDoList.js
+++ assets/scripts/script-toDoList.js
@@ -1,3 +1,18 @@
 function irAToDo(){
     document.location.href = "../pages/toDoList.html";
 }
+
+function agregarTarea(){
+    var tareaNueva= document.getElementById("txtTarea").value;
+    var table = document.getElementById("tblTareas");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = tareaNueva;
+    var cell2 = row.insertCell(1);
+    cell2.innerHTML = '<button>EDITAR</button> <button>ELIMINAR</button>';
+    document.getElementById("tblTareas").value = "";
+}
+
+function editarTarea(){
+    
+}
